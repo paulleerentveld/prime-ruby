14b8e8abea53b2553b1f1215bae2cca7535eafc3
paulleerentveld <paul@veldnet.com.au>
2022-06-16 18:24:24 +1000

    lab done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+
+    if num == 0 || num == 1 || num < 0
+    return false
+    elsif num == 2
+        return true
+    else 
+        range_end = num-1
+        (2..range_end).none? { |i| (num % i).zero? }
+    end 
+end
+
+
+puts prime?(3)
+
