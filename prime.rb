dbb8355d4ad03960ade396836a5c9e2a493e4c7c
paulleerentveld <paul@veldnet.com.au>
2022-06-17 14:03:12 +1000

    comparing learn-co solution and added benchmark

--- prime.rb
+++ prime.rb
@@ -1,16 +1,25 @@
-# Add  code here!
-def prime?(num)
+require 'benchmark'
 
-    if num == 0 || num == 1 || num < 0
-    return false
-    elsif num == 2
-        return true
-    else 
-        range_end = num-1
-        (2..range_end).none? { |i| (num % i).zero? }
-    end 
+# My Solution
+def prime?(num)
+    num == 0 || num == 1 || num < 0 ? false : (2..num-1).none? { |x| num % x ==0 }
 end
 
+puts Benchmark.measure {prime?(105557123)}
+puts prime?(105557123)
 
-puts prime?(3)
+
+# Learn-co Solution
+def prime_learnco?(num)
+    if num < 0 or num == 0 or num == 1
+      return false
+    else
+      (2..num-1).to_a.all? do |possible_factor|
+        num % possible_factor != 0
+      end
+    end
+end
+  
+puts Benchmark.measure {prime_learnco?(105557123)}
+puts prime_learnco?(105557123)
 
